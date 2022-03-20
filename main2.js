912fd6d3657325a821d5a73d00ba5c6ab1b8ddb9
Griger <grigerratia@gmail.com>
2022-03-19 20:23:40 -0400

    Finalizado el proyecto

--- main2.js
+++ main2.js
@@ -23,20 +23,54 @@ function deepCopy(subject) {
     for (key in subject) {
         const keyIsObject = isObject(subject[key]);
 
-    if (keyIsObject) {
-        copySubject[key] = deepCopy(subject[key]);
-    } else {
-        if (subjectIsArray) {
-            copySubject.push(subject[key]);
+        if (keyIsObject) {
+            copySubject[key] = deepCopy(subject[key]);
         } else {
-            copySubject[key] = subject[key];
+            if (subjectIsArray) {
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
         }
-    }
     }
 
     return copySubject;
 }
 
+function SuperObject() {}
+    SuperObject.isObject = function (subject) {
+        return typeof subject == "object";
+    }
+    SuperObject.deepCopy = function (subject) {
+        let copySubject;
+
+        const subjectIsObject = isObject(subject);
+        const subjectIsArray = isArray(subject);
+
+        if (subjectIsArray) {
+            copySubject = [];
+        } else if (subjectIsObject) {
+            copySubject = {};
+        } else {
+            return subject;
+        }
+
+        for (key in subject) {
+            const keyIsObject = isObject(subject[key]);
+
+        if (keyIsObject) {
+            copySubject[key] = deepCopy(subject[key]);
+            } else {
+                if (subjectIsArray) {
+                    copySubject.push(subject[key]);
+                } else {
+                    copySubject[key] = subject[key];
+                }
+            }
+        }
+
+        return copySubject;
+    }
 
 function requiredParam(param) {
     throw new Error(param + " es obligatorio");
@@ -48,29 +82,6 @@ function LearningPath({
 }) {
     this.name = name;
     this.courses = courses;
-
-    // const private = {
-    //   "_name": name,
-    //   "_courses": courses,
-    // };
-
-    // const public = {
-    //   get name() {
-    //     return private["_name"];
-    //   },
-    //   set name(newName) {
-    //     if (newName.length != 0) {
-    //       private["_name"] = newName;
-    //     } else {
-    //       console.warn("Tu nombre debe tener al menos 1 caracter");
-    //     }
-    //   },
-    //   get courses() {
-    //     return private["_courses"];
-    //   },
-    // };
-
-    // return public;
 }
 
 function Student({
@@ -93,65 +104,26 @@ function Student({
         facebook,
     };
 
-    if (isArray(learningPaths)) {
-        this._learningPaths = [];
+    const private = {
+        "_learningPaths": [],
+    };
 
-        for (learningPathIndex in learningPaths) {
-            if (learningPaths[learningPathIndex] instanceof LearningPath) {
-            this.learningPaths.push(learningPaths[learningPathIndex]);
+    Object.defineProperty(this, "learningPaths", {
+        get() {
+            return private["_learningPaths"];
+        },
+        set(newLp) {
+            if (newLp instanceof LearningPath) {
+                private["_learningPaths"].push(newLp);
+            } else {
+                console.warn("Alguno de los LPs no es una instancia dell prototipo LearningPath");
             }
-        }
+        },
+    });
+
+    for (learningPathIndex in learningPaths) {
+        this.learningPaths = learningPaths[learningPathIndex];
     }
-
-
-
-    // const private = {
-    //   "_name": name,
-    //   "_learningPaths": learningPaths,
-    // };
-
-    // const public = {
-    //   email,
-    //   age,
-    //   approvedCourses,
-    //   socialMedia: {
-    //     twitter,
-    //     instagram,
-    //     facebook,
-    //   },
-    //   get name() {
-    //     return private["_name"];
-    //   },
-    //   set name(newName) {
-    //     if (newName.length != 0) {
-    //       private["_name"] = newName;
-    //     } else {
-    //       console.warn("Tu nombre debe tener al menos 1 caracter");
-    //     }
-    //   },
-    //   get learningPaths() {
-    //     return private["_learningPaths"];
-    //   },
-    //   set learningPaths(newLP) {
-    //     if (!newLP.name) {
-    //       console.warn("Tu LP no tiene la propiedad name");
-    //       return;
-    //     }
-
-    //     if (!newLP.courses) {
-    //       console.warn("Tu LP no tiene courses");
-    //       return;
-    //     }
-
-    //     if (!isArray(newLP.courses)) {
-    //       console.warn("Tu LP no es una lista (*de cursos)");
-    //       return;
-    //     }
-    //     private["_learningPaths"].push(newLP);
-    //   },
-    // };
-
-    // return public;
 }
 
 const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
@@ -160,7 +132,7 @@ const juan = new Student({
     email: "juanito@frijoles.co",
     name: "Juanito",
     learningPaths: [
-    escuelaWeb,
-    escuelaData,
+        escuelaWeb,
+        escuelaData,
     ],
 });
